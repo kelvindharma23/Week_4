c5ab2b88fa37d6f84a4f32e78c77f33313b98dc6
kelvindharma23 <kelvin.53180061@stu.untar.ac.id>
2020-09-25 22:12:43 +0700

    java Script

---
+++ 01 Html&Git/script.js
@@ -0,0 +1,6 @@
+function cangelinestyle(){
+    document.getElementById("Line1").className = "red";
+}
+function changestyle(iid,classname){
+     document.getElementById(id).className = classname;
+}
